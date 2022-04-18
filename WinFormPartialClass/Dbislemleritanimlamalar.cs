c0bac350118532edf1a264b2cba53de70fa7ea4b
Mehmet Sunay <mehmetsunay@windowslive.com>
2022-04-18 16:25:01 +0300

    Partial-Class-Kullanimi
    Partial-Class-Kullanimi

---
+++ WinFormPartialClass/Dbislemleritanimlamalar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormPartialClass
+{
+   public partial class Dbislemleri
+    {
+        public int kdvorani = 18;
+        public string kargocu = "Getir Kargo";
+
+
+
+    }
+}
